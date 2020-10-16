28ff4e98ae9f7e9687a2241376fa4bbb0035a8c1
Matioma <Artiom.Matievschi@gmail.com>
2020-10-16 14:25:30 +0200

    Renamed road editor tooling class

--- Assets/Scripts/MyScripts/Editor/RoadEditor.cs
+++ Assets/Scripts/MyScripts/Editor/RoadEditor.cs
@@ -25,18 +25,13 @@ public class RoadEditor : BuilderGenerateButtons
 
 
         EditorGUI.BeginChangeCheck();
-        //Vector3 startPosition = target.startPosition;
         Vector3 start = Handles.PositionHandle(road.streetParameters.startPosition, Quaternion.identity);
         Vector3 end = Handles.PositionHandle(road.streetParameters.endPosition, Quaternion.identity);
-        //Vector3 newTargetPosition = Handles.PositionHandle();
+     
         if (EditorGUI.EndChangeCheck())
         {
             road.streetParameters.startPosition = start;
             road.streetParameters.endPosition = end;
-            //Debug.lo
-            //Undo.RecordObject(example, "Change Look At Target Position");
-            //example.targetPosition = newTargetPosition;
-            //example.Update();
         }
     }
 }
