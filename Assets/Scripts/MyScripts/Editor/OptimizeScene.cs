30c0467c8984b76e01f3174f6cc63f1fcc4970e6
Matioma <Artiom.Matievschi@gmail.com>
2020-11-06 16:21:15 +0100

    Start building the city, first building

---
+++ Assets/Scripts/MyScripts/Editor/OptimizeScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public static  class  OptimizeScene
+{
+    [MenuItem("Optimize Scene/MergeMeshes ")]
+    public static void MergeMeshes() {
+        Debug.Log("Merge");
+
+        CombineMeshes[] combineMeshes = Resources.FindObjectsOfTypeAll<CombineMeshes>();
+        foreach(var mesh in combineMeshes) {
+            mesh.Optimize();
+        }
+        //Debug.Log(combineMeshes.Length);
+    }
+}
